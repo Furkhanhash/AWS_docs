8bc5cea3755c225904130ac1259c3047aa6574bd
Furkhanhash <marshalrap96@gmail.com>
2024-05-22 10:32:51 -0400

    Add files via upload

---
+++ idle_instance.py
@@ -0,0 +1,37 @@
+import boto3
+from datetime import datetime
+from pytz import timezone
+
+
+def ec2_change(status,ids):
+    if status=="start":
+        response=ec2.start_instances(InstanceIds=[s])
+        print(response)
+
+
+# Initialize a session using Boto3
+#session = boto3.Session(region_name='us-east-1')
+ec2=boto3.client('ec2',region_name='us-east-2')
+# Initialize an S3 client
+#s3 = session.client('s3')
+s="i-0ed2425feb3013168"
+
+
+tz=timezone('US/Eastern')
+current_t=datetime.now(tz)
+
+
+dw=current_t.isoweekday()
+
+if dw==4:
+    ec2_change("start",s)
+elif dw==3:
+    print("Here")
+
+
+# List all buckets
+#response = s3.list_buckets()
+
+# Print the bucket names
+#for bucket in response['Buckets']:
+#    print(bucket['Name'])
