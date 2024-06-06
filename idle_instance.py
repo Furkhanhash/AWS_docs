b37fb368589074f3a2fd52b4e5015f7a3d9ff867
Furkhanhash <marshalrap96@gmail.com>
2024-06-06 16:32:45 -0400

    Update idle_instance.py

--- idle_instance.py
+++ idle_instance.py
@@ -3,35 +3,26 @@ from datetime import datetime
 from pytz import timezone
 
 
-def ec2_change(status,ids):
-    if status=="start":
-        response=ec2.stop_instances(InstanceIds=[s])
-        print(response)
+def ec2_change(status, ids):
+    ec2 = boto3.client('ec2', region_name='us-east-2')
+    if status == "start":
+        response = ec2.start_instances(InstanceIds=ids)
+    elif status == "stop":
+        response = ec2.stop_instances(InstanceIds=ids)
+    return response
 
 
-# Initialize a session using Boto3
-#session = boto3.Session(region_name='us-east-1')
-ec2=boto3.client('ec2',region_name='us-east-2')
-# Initialize an S3 client
-#s3 = session.client('s3')
-s="i-0ed2425feb3013168"
+def ec2_optimize(event, context):
+    s = ["i-0ed2425feb3013168", "i-020d74232cc101f04"]
+    tz = timezone('US/Eastern')
+    current_t = datetime.now(tz)
+    dw = current_t.isoweekday()
+    print(dw)
+    if dw == 1:  # Monday
+        ec2_change("start", s)
+    elif dw == 5:  # Friday
+        ec2_change("stop", s)
 
 
-tz=timezone('US/Eastern')
-current_t=datetime.now(tz)
-
-
-dw=current_t.isoweekday()
-
-if dw==2:
-    ec2_change("start",s)
-elif dw==4:
-    print("Here")
-
-
-# List all buckets
-#response = s3.list_buckets()
-
-# Print the bucket names
-#for bucket in response['Buckets']:
-#    print(bucket['Name'])
+if __name__ == "__main__":
+    ec2_optimize(None, None)
